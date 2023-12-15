227b54e98ba1ed2c4a8948b48ec8d3fdd465b70f
Jagruti kansurkar <Jagrutivk25@gmail.com>
2023-12-15 17:15:14 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ function App() {
   return (
     <div>
       <h1> Hello World.... </h1>
-      <h3>Welcome</h3>
+      <h3>This is first pipeline I have created and it is working fine...</h3>
+      <h5>Thank You All.</h5>
+      <h6>From Jagruti.</h6>
     </div>
   );
 }
